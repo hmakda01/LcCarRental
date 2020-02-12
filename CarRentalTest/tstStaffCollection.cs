447869adec7f6b2334dde19ca6ee15a400657688
hmkada01 <P2429815@my365.dmu.ac.uk>
2020-02-12 12:53:20 +0000

    Added The Testing

---
+++ CarRentalTest/tstStaffCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarRentalTest
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
