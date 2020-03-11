8d67b71695d2b1667a2a7374c48c2002980861bd
p2507439 <p2507439@GWHXX633WIN017W>
2020-03-11 15:14:02 +0000

    completed work

---
+++ CarRentalTest/tstOrderCollection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using CarRentalClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CarRentalTest
+{
+    [TestClass]
+    public class tstOrderCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            Assert.IsNotNull(AllOrders);
+        }
+        [TestMethod]
+        public void OrderListOk()
+        {
+
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            List<clsOrder> TestList = new List<clsOrder>();
+            clsOrder TestItem = new clsOrder();
+            TestItem.CarReg = 1;
+            TestItem.CustomerID = 1;
+            TestItem.OrderID = 1;
+            TestItem.StaffID = 1;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Email = "";
+            TestItem.Price = 1;
+            TestList.Add(TestItem);
+            AllOrders.OrderList = TestList;
+            Assert.AreEqual(AllOrders.OrderList, TestList);
+        }
+
+        [TestMethod]
+        public void ListAndCountOk()
+        {
+
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            List<clsOrder> TestList = new List<clsOrder>();
+            clsOrder TestItem = new clsOrder();
+            TestItem.CarReg = 1;
+            TestItem.CustomerID = 1;
+            TestItem.OrderID = 1;
+            TestItem.StaffID = 1;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Email = "";
+            TestItem.Price = 1;
+            TestList.Add(TestItem);
+            AllOrders.OrderList = TestList;
+            Assert.AreEqual(AllOrders.OrderList, TestList);
+        }
+
+
+
+
+
+
+
+    }
+}
